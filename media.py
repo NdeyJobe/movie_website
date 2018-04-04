f71bf9b07ef8d88e84d4820e64c1ba9a1e554ff9
Ndey Isatou Jobe <isa2jobe@gmail.com>
2018-04-04 12:03:43 +0200

    Add movie list module

---
+++ media.py
@@ -0,0 +1,15 @@
+import webbrowser
+
+class Movie () :
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, movie_reviews):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.reviews_url = movie_reviews
+
+    def show_trailer (self) :
+        webbrowser.open(self.trailer_youtube_url)
+
+    def show_reviews(self):
+        webbrowser.open(self.reviews_url)
